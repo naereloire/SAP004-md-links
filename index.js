a16e1a8a51b06b2a1387cba588558e131eb86a30
naereloire <naere.loire@gmail.com>
2020-07-02 21:10:16 -0300

    implem estrutura inicial cli md links

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-module.exports = () => {
-  // ...
-};
+#!/usr/bin/env node
+
+// module.exports = () => {
+//   // ...
+// };
+const program = require("commander");
+const package = require("./package.json");
+
+program.version(package.version);
+
+program
+  .command("md-links <path> [--validate] [--stats]")
+  .description("Escaneia md file")
+  .action((path) => {
+    console.log(path);
+  });
+program.parse(process.argv);
