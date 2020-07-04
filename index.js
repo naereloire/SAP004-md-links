0d4921c89225d07b2273d43528d2fd38ac5b6ec8
naereloire <naere.loire@gmail.com>
2020-07-04 16:36:36 -0300

    add fs para verificar se é file ou dir

--- index.js
+++ index.js
@@ -8,14 +8,23 @@ const program = require("commander");
 const package = require("./package.json");
 const fs = require("fs");
 
-let currentPath = "/home/Área de Trabalho/laboratoria/SAP004-md-links";
-const verifyFile = () => {
+const verifyPath = (currentPath) => {
   fs.stat(currentPath, (err, stats) => {
     if (!err) {
       if (stats.isFile()) {
-        console.log("is file?" + stats.isFile());
+        fs.readFile(currentPath, (err, data) => {
+          if (err) {
+            throw err;
+          }
+          console.log(data);
+        });
       } else if (stats.isDirectory()) {
-        console.log("is directory?" + stats.isDirectory());
+        fs.readdir(currentPath, (err, files) => {
+          if (err) {
+            throw err;
+          }
+          console.log(files);
+        });
       }
     } else {
       throw err;
@@ -31,23 +40,16 @@ program
   .option("-v, --validate", "Validate link")
   .option("-s, --stats", "Statical of links in md file")
   .action((path, options) => {
+    verifyPath(path);
     switch (true) {
       case options.validate && options.stats:
-        verifyFile();
-
         break;
       case options.validate:
-        verifyFile();
-
         break;
       case options.stats:
-        verifyFile();
-
         break;
       default:
         console.log(path + " não deu options");
     }
   });
 program.parse(process.argv);
-
-// todo;
