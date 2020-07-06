dd341b398b6f52e4f18a2c092ce098021183b850
naereloire <naere.loire@gmail.com>
2020-07-05 23:32:10 -0300

    dev inicial validate link

--- index.js
+++ index.js
@@ -5,27 +5,48 @@
 // };
 
 const regexLinks = /\[[^\]]*\]\(http.*\)/g;
-const regexSplitLink = /\[|\]|\(|\)/g;
-
+const regexSplitLink = /^\[|\]\(|\)$/g;
 const program = require("commander");
 const package = require("./package.json");
 const fs = require("fs");
-// const { text } = require("figlet");
+const https = require("https");
+const http = require("http");
+const { text } = require("figlet");
+
+const validateLink = (objectLink) => {
+  link = objectLink.href;
+  if (link.startsWith("https")) {
+    https.get(link, (res) => {
+      objectLink["statusCode:"] = res.statusCode;
+      console.log("Message:", res.statusMessage);
+    });
+  } else {
+    http.get(link, (res) => {
+      objectLink["statusCode:"] = res.statusCode;
+      console.log("Message:", res.statusMessage);
+    });
+  }
+  console.log(objectLink);
+  return objectLink;
+};
 
 const findLink = (data) => {
   const arrayLinks = data.match(regexLinks);
-  const objectLinks = [];
+  const arrayObjectLinks = [];
   for (const element of arrayLinks) {
     let arraySplitLinks = element.split(regexSplitLink);
     arraySplitLinks = arraySplitLinks.filter((element) => {
       return element !== "";
     });
-    objectLinks.push({
+    let objectLink = {
       text: arraySplitLinks[0],
       href: arraySplitLinks[1],
-    });
+    };
+    objectLink = validateLink(objectLink);
+
+    arrayObjectLinks.push(objectLink);
   }
-  return objectLinks;
+  return arrayObjectLinks;
 };
 
 const readDirectory = (err, files, currentPath) => {
@@ -53,7 +74,8 @@ const readArchive = (err, data, path) => {
   }
   const findLinkReturn = findLink(data);
   for (const element of findLinkReturn) {
-    console.log(`${path} ${element.href} ${element.text}`);
+    // console.log(element);
+    // console.log(`${path} ${element.href} ${element.text}`);
   }
 };
 
