cd84a8cefd81751f8644913895133431d59e0c72
naereloire <naere.loire@gmail.com>
2020-07-11 21:20:55 -0300

    dev inicial test func mdLinks

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -7,6 +7,6 @@ describe("mdLinks", () => {
     expect(typeof mdLinks).toBe("function");
   });
   it("Deveria criar um array de objetos e retorna-lo em uma promise", () => {
-    expect(mdLinks(path));
+    expect(mdLinks(path)).tobe();
   });
 });
